721d3ce3ea4df727e9d64f2666606784c36256bc
Alexander Sevostianov <sevostianov@ecopsy.ru>
2024-04-26 00:14:15 +0300

    better logging for solver

--- solver/utils.py
+++ solver/utils.py
@@ -1,11 +1,13 @@
 from functools import wraps
 from datetime import datetime
+import logging
 
 def timed(func):
     @wraps(func)
-    def timed_func(*args, **kwargs):
-        start_time = datetime.now()
-        ret = func(*args, **kwargs)
-        print(f'{func.__name__} - {(datetime.now() - start_time).total_seconds():.4f}')
-        return ret
-    return timed_func
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        result = func(*args, **kwargs)
+        end = datetime.now()
+        logging.info(f"{args[0].__class__.__name__}.{func.__name__} ran in: {end - start} for puzzle: {args[0].input_string}")
+        return result
+    return wrapper
